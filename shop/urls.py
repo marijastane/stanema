e4407d1a50fb04213048a606428b008f0bf03ff0
marijastane <daublyte.marija@gmail.com>
2021-03-11 14:25:21 +0200

    Naujas

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,9 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
 app_name = 'shop'
 
 urlpatterns = [
-    path('', views.)
+    path('/login', LoginView.as_view(), name='login'),
+    path('/logout', LogoutView.as_view(), name='logout'),
 ]
